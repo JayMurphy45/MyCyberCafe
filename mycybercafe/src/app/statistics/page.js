da2d61e5faf7b3ae65d19d2a9859e5ff7c4b8849
JayMurphy45 <113984910+JayMurphy45@users.noreply.github.com>
2023-12-06 13:58:09 +0000

    Statistics and monitor page
    06/12/23

--- mycybercafe/src/app/statistics/page.js
+++ mycybercafe/src/app/statistics/page.js
@@ -5,8 +5,40 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Button from '@mui/material/Button';
 import Link from 'next/link';
+import { Bar } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    BarElement,
+    CategoryScale,
+    LinearScale, //this is our y axis
+    Tooltip,
+    Legend
 
+} from 'chart.js'
+
+ChartJS.register(
+    BarElement,
+    CategoryScale,
+    LinearScale,
+    Tooltip,
+    Legend
+)
 export default function CenteredTabs() {
+
+    const data = {
+        labels: ['YouTube', 'Stackoverflow', 'GPT'],
+        datasets: [
+            {
+                label: 'Seconds on Webpage',
+                data: [5, 7, 1],
+                backgroundColor: 'red',
+                borderColor: 'black',
+                borderWidth: 1,
+            }
+        ]
+    }
+
+    const options = {}
     const [value, setValue] = React.useState(0);
 
     const handleChange = (event, newValue) => {
@@ -30,13 +62,12 @@ export default function CenteredTabs() {
 
                 </Tabs>
             </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-                <Button variant="contained" disableElevation>
-                    Monitor
-                </Button>
-            </Box>
+            <div>
+                <Bar
+                    data={data}
+                    options={options}
+                ></Bar>
+            </div>
         </Box>
     );
 }
-
-
