20a811ea7b3982a480c4a7da3f8e80ece6246eff
JayMurphy45 <113984910+JayMurphy45@users.noreply.github.com>
2024-01-31 10:17:29 +0000

    database connectivity via mongoose

--- mycybercafe/src/app/api/login/route.js
+++ mycybercafe/src/app/api/login/route.js
@@ -1,51 +1,39 @@
 export async function GET(req, res) {
 
-    // Make a note we are on
-    // the api. This goes to the console.
-    console.log("in the api page")
-  
-  
-    // get the values
-    // that were sent across to us.
-    const { searchParams } = new URL(req.url)
-    const email = searchParams.get('email')
-    const pass = searchParams.get('pass')
-  
-    console.log(email);
-    console.log(pass);
-  
-  
-   
-  
-    // =================================================
-  const { MongoClient } = require('mongodb');
-  const url = 'mongodb+srv://b00143682:pass@cluster0.2tcd7yu.mongodb.net/?retryWrites=true&w=majority';
-  const client = new MongoClient(url);
-  const dbName = 'app'; // database name
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('register'); // collection name
-  
-  
-  const findResult = await collection.find({"username":  email}).toArray();
-  
-  
-  
-  console.log('Found documents =>', findResult);
-  let valid = false
-  if(findResult.length >0 ){
-    valid = true;
-    console.log("login valid")
-  } else {
-    valid = false;
-    console.log("login invalid")
-  }
-  
-  
+  // Make a note we are on
+  // the api. This goes to the console.
+  console.log("in the api page")
+
+
+  // get the values
+  // that were sent across to us.
+  const { searchParams } = new URL(req.url)
+  const email = searchParams.get('email')
+  const pass = searchParams.get('pass')
+
+  console.log(email);
+  console.log(pass);
+
+
+
+
+  // =================================================
+  const mongoose = require("mongoose")
+
+  const dburl = "mongodb+srv://jamie:pass@cluster0.bjrqopf.mongodb.net/?retryWrites=true&w=majority"
+
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  };
+
+  mongoose.connect(dburl, connectionParams).then(() => {
+    console.log("Connected to the DB");
+  }).catch((e) => {
+    console.log("Error", e);
+  });
   //==========================================================
-  
-    // at the end of the process we need to send something back.
-  return Response.json({ "data":"" + valid + ""})
-  }
-  
+
+  // at the end of the process we need to send something back.
+  return Response.json({ "data": "" + valid + "" })
+}
--- mycybercafe/src/app/api/register/route.js
+++ mycybercafe/src/app/api/register/route.js
@@ -1,50 +1,48 @@
 export async function GET(req, res) {
 
-    // Make a note we are on
-    // the api. This goes to the console.
-    console.log("in the api page")
-  
-  
-    // get the values
-    // that were sent across to us.
-    const { searchParams } = new URL(req.url)
-    const email = searchParams.get('email')
-    const pass = searchParams.get('pass')
-    const pass2 = searchParams.get('pass2')
-    const DOB = searchParams.get('DOB')
-    const PhoneNumber = searchParams.get('PhoneNumber')
-    console.log(email);
-    console.log(pass);
-    console.log(pass2);
-    console.log(DOB);
-    console.log(PhoneNumber);
-  
-   
-  
-    // =================================================
-  const { MongoClient } = require("mongodb");
-  const url = "mongodb+srv://b00143682:pass@cluster0.2tcd7yu.mongodb.net/?retryWrites=true&w=majority";
-  const client = new MongoClient(url);
-  const dbName = 'app'; // database name
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('register'); // collection name
-  const findResult = await collection.insertOne({
-    username: email,
-    pass: pass,
-    pass2: pass2,
-    DOB: DOB,
-    PhoneNumber: PhoneNumber
+  // Make a note we are on
+  // the api. This goes to the console.
+  console.log("in the api page")
 
+
+  // get the values
+  // that were sent across to us.
+  const { searchParams } = new URL(req.url)
+  const email = searchParams.get('email')
+  const pass = searchParams.get('pass')
+  const pass2 = searchParams.get('pass2')
+  const DOB = searchParams.get('DOB')
+  const PhoneNumber = searchParams.get('PhoneNumber')
+  console.log(email);
+  console.log(pass);
+  console.log(pass2);
+  console.log(DOB);
+  console.log(PhoneNumber);
+
+
+
+  // =================================================
+  const mongoose = require("mongoose")
+
+  const dburl = "mongodb+srv://jamie:pass@cluster0.bjrqopf.mongodb.net/?retryWrites=true&w=majority"
+
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  };
+
+  mongoose.connect(dburl, connectionParams).then(() => {
+    console.log("Connected to the DB");
+  }).catch((e) => {
+    console.log("Error", e);
   });
-  
-  
-  
-  
+
+
+
+
   let valid = true;
-  
-  
-    // at the end of the process we need to send something back.
-  return Response.json({ "data":"" + valid + ""})
-  }
+
+
+  // at the end of the process we need to send something back.
+  return Response.json({ "data": "" + valid + "" })
+}
