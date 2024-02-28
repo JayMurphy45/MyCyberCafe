51a8d7f1aaae68444594f01452e2f5ef54b5a1b3
B00146242 <B00146242@mytudublin.ie>
2024-02-28 17:17:40 +0000

    Update on the Popup.js
    For the Collecting the url

--- mycybercafe/src/exstension/popup.js
+++ mycybercafe/src/exstension/popup.js
@@ -7,3 +7,23 @@ document.getElementById('pauseBtn').addEventListener('click', function() {
     // Send message to background script to pause the timer
     chrome.runtime.sendMessage({ action: 'pauseBtn' });
 });
+
+console.log('This is a popup!');
+    var button = document.querySelector('button')
+    button.addEventListener("click", () => {
+    alert("button clicked");
+    // get the open tab url
+    chrome.tabs.query(
+        {active:true},
+        tabs=>{
+            const tab=tabs[0];
+            console.log("URL:", tab.url)
+            alert(tab.url)
+            console.log("Getting ready to save the record");       
+            // get the current time in EPOCH format.
+            var currentTime = new Date().valueOf();
+            alert(currentTime)
+            fetch('http://localhost:5001?username=xx&url=11111&currentTime='+currentTime)
+        }
+        )
+    });
