01d278e2bc002a419955485e1e4fd248c776df38
JayMurphy45 <B00143682@mytudublin.ie>
2024-03-01 18:24:25 +0000

    1
    1

--- mycybercafe/src/exstension/content.js
+++ mycybercafe/src/exstension/content.js
@@ -1,27 +1,18 @@
-
 const article = document.querySelector("article");
 
 let currentURL = window.location;
 
-<<<<<<< Updated upstream
-//alert("current address is: " + currentURL);
-/*
-setTimeout(() => {
-    fetch('http://localhost:3000/api/SaveToDb?username=john&url=' + encodeURIComponent(currentURL))
-        .then(response => {
-            // Handle the response here
-        })
-        .catch(error => {
-            // Handle any errors here
-        });
-}, 5000); // 5000 milliseconds = 5 seconds
-*/
-=======
 alert("current address is: " + currentURL);
 
-fetch(
-  "http://localhost:3000/api/SaveToDb?username=john&url=${encodeURIComponent(currentURL)}"
-);
-fetch("http://localhost:3000/api/SaveToDb?username=john&url=" + currentURL); //Kyle's version
-content.js;
->>>>>>> Stashed changes
+setTimeout(() => {
+  fetch(
+    "http://localhost:3000/api/SaveToDb?username=john&url=" +
+      encodeURIComponent(currentURL)
+  )
+    .then((response) => {
+      // Handle the response here
+    })
+    .catch((error) => {
+      // Handle any errors here
+    });
+}, 5000); // 5000 milliseconds = 5 seconds
