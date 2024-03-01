7cf09382b4f27c776d7ee7e289c0841585aefa66
JayMurphy45 <B00143682@mytudublin.ie>
2024-03-01 18:23:42 +0000

    1
    1

--- mycybercafe/src/exstension/content.js
+++ mycybercafe/src/exstension/content.js
@@ -3,6 +3,7 @@ const article = document.querySelector("article");
 
 let currentURL = window.location;
 
+<<<<<<< Updated upstream
 //alert("current address is: " + currentURL);
 /*
 setTimeout(() => {
@@ -15,3 +16,12 @@ setTimeout(() => {
         });
 }, 5000); // 5000 milliseconds = 5 seconds
 */
+=======
+alert("current address is: " + currentURL);
+
+fetch(
+  "http://localhost:3000/api/SaveToDb?username=john&url=${encodeURIComponent(currentURL)}"
+);
+fetch("http://localhost:3000/api/SaveToDb?username=john&url=" + currentURL); //Kyle's version
+content.js;
+>>>>>>> Stashed changes
